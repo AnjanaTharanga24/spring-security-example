d3ccdae15a8083b3e6a02013fd23eb333c0218cf
AnjanaTharanga24 <anjana24myprof@gmail.com>
2024-07-30 04:57:22 +0000

    new update on the spring security example

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <div style={{marginTop:"200px" , marginLeft:"500px"}}>
+        <h1 className="fs-1">This is Spring Security example home page</h1>
+      </div>
     </div>
-  )
+  );
 }
